33f5616fc19365295bf8238b84b021ff8823c75a
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-12 23:09:44 +0300

    Add Tenant database model initial commit

---
+++ PropertyPro.Infrastructure/Models/Tenant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Models
+{
+    public class Tenant
+    {
+        public Tenant()
+        {
+            this.Bookings = new List<Booking>();
+            this.Reviews = new List<Review>();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(User))]
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public ICollection<Booking>? Bookings { get; set; }
+
+        public ICollection<Review>? Reviews { get; set; }
+    }
+}
+
+//update-database -verbose -Project PropertyPro.Infrastructure -StartupProject PropertyPro
