4f63b8819dda6f32317c55b3d8f72486ec63e5f4
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-12 23:05:15 +0300

    Add PropertyProDbContext initial commit

---
+++ PropertyPro.Infrastructure/Data/PropertyProDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PropertyPro.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Data
+{
+    public class PropertyProDbContext : DbContext
+    {
+        public PropertyProDbContext(DbContextOptions<PropertyProDbContext> options) : base(options)
+        {
+        }
+
+        public PropertyProDbContext()
+        {
+        }
+
+        public DbSet<User>? Users { get; set; }
+
+        public DbSet<Tenant>? Tenants { get; set; }
+
+        public DbSet<Landlord>? Landlords { get; set; }
+
+        public DbSet<Booking>? Bookings { get; set; }
+
+        public DbSet<Property>? Properties { get; set; }
+
+        public DbSet<Review>? Reviews { get; set; }
+    }
+}
