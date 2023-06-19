f823b61f1d10794d04e6230fe5c94f5a7fef2468
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-19 13:00:40 +0300

    Add cors policy for local server and identity in the IOC

--- PropertyPro/Extensions/ServiceCollectionExtension.cs
+++ PropertyPro/Extensions/ServiceCollectionExtension.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using PropertyPro.Infrastructure.Common;
 using PropertyPro.Infrastructure.Data;
+using PropertyPro.Infrastructure.Models;
 
 namespace PropertyPro.Extensions
 {
@@ -11,9 +12,27 @@ namespace PropertyPro.Extensions
         public static void AddServices(this IServiceCollection services)
         {
             services.AddControllers();
-            services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
-            
+            services.AddIdentity<User, IdentityRole<Guid>>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+            })
+                .AddEntityFrameworkStores<PropertyProDbContext>()
+                .AddDefaultTokenProviders();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("LocalServer", policy =>
+                {
+                    policy.WithOrigins("http://127.0.0.1:5500")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+           
+
             services.AddScoped<IRepository,Repository>();
         }
     }
