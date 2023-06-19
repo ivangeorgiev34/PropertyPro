ce9e62163f7b7bf2dedd8177ad3699fea8259f68
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-19 13:01:19 +0300

    Add authentication with JWT to the IOC

--- PropertyPro/Program.cs
+++ PropertyPro/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using PropertyPro.Extensions;
 using PropertyPro.Infrastructure.Data;
+using System.Text;
 
 namespace PropertyPro
 {
@@ -16,6 +19,27 @@ namespace PropertyPro
             builder.Services.AddDbContext<PropertyProDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("PropertyProConnectionString"));
+            });
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            //Adding Jwt Bearer
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+                };
             });
 
             ServiceCollectionExtension.AddServices(builder.Services);
@@ -34,6 +58,7 @@ namespace PropertyPro
             app.UseAuthorization();
             app.UseAuthentication();
 
+            app.UseCors("LocalServer");
 
             app.MapControllers();
 
