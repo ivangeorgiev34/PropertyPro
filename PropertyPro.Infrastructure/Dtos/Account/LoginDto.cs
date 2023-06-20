af7690f839ab6946636738210774c48a34eb1ea7
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-20 22:47:46 +0300

    Add RegisterDto and LoginDto initial commit

---
+++ PropertyPro.Infrastructure/Dtos/Account/LoginDto.cs
@@ -0,0 +1,20 @@
+﻿using PropertyPro.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Account
+{
+    public class LoginDto
+    {
+        [EmailAddress(ErrorMessage =InfrastructureConstants.LoginDto.LOGIN_DTO_EMAIL_INVALID_ERROR_MESSAGE)]
+        [Required(ErrorMessage =InfrastructureConstants.LoginDto.LOGIN_DTO_EMAIL_REQUIRED_ERROR_MESSAGE)]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.LoginDto.LOGIN_DTO_PASSWORD_REQUIRED_ERROR_MESSAGE)]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ PropertyPro.Infrastructure/Dtos/Account/RegisterDto.cs
@@ -0,0 +1,41 @@
+﻿using PropertyPro.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Account
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = InfrastructureConstants.User.FIRST_NAME_REQUIRED_ERROR_MESSAGE)]
+        public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.User.MIDDLE_NAME_REQUIRED_ERROR_MESSAGE)]
+        public string MiddleName { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.User.LAST_NAME_REQUIRED_ERROR_MESSAGE)]
+        public string LastName { get; set; } = null!;
+
+        public string? Gender { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.User.AGE_REQUIRED_ERROR_MESSAGE)]
+        [Range(InfrastructureConstants.User.AGE_MIN_VALUE, InfrastructureConstants.User.AGE_MAX_VALUE, ErrorMessage = InfrastructureConstants.User.AGE_REQUIRED_ERROR_MESSAGE)]
+        public int Age { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Username { get; set; }
+
+        [Phone(ErrorMessage =InfrastructureConstants.RegisterDto.REGISTER_DTO_PHONE_INVALID_ERROR_MESSAGE)]
+        public string? PhoneNumber { get; set; }
+
+        public string? Password { get; set; }
+
+        [Compare(nameof(Password),ErrorMessage =InfrastructureConstants.RegisterDto.REGISTER_DTO_PASSWORDS_MATCH_ERROR_MESSAGE)]
+        public string? ConfirmPassword { get; set; }
+
+    }
+}
