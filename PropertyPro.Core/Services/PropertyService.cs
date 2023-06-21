25c526b392b1088bb9a0c4443ed8cf339ecefc14
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-21 22:43:07 +0300

    Add property service initial commit

---
+++ PropertyPro.Core/Services/PropertyService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using PropertyPro.Core.Contracts;
+using PropertyPro.Infrastructure.Common;
+using PropertyPro.Infrastructure.Dtos.Property;
+using PropertyPro.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Core.Services
+{
+    public class PropertyService : IPropertyService
+    {
+        private readonly IRepository repo;
+        public PropertyService(IRepository _repo)
+        {
+            this.repo = _repo;
+        }
+        public async Task<List<GetAllPropertiesDto>> GetAllPropertiesAsync()
+        {
+            var properties = await repo.AllReadonly<Property>()
+                 .Select(p => new GetAllPropertiesDto
+                 {
+                     Id = p.Id,
+                     Title = p.Title,
+                     Type = p.Type,
+                     BathroomsCount = p.BathroomsCount,
+                     BedroomsCount = p.BedroomsCount,
+                     BedsCount = p.BedsCount,
+                     Country = p.Country,
+                     Description = p.Description,
+                     MaxGuestsCount = p.MaxGuestsCount,
+                     Town = p.Town
+                 })
+                 .ToListAsync();
+
+            return properties;
+        }
+    }
+}
