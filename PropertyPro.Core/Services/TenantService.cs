03d2c70432e69abf822fbd12b008ac2e93cad933
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-22 14:59:28 +0300

    Add TenantService initial commit

---
+++ PropertyPro.Core/Services/TenantService.cs
@@ -0,0 +1,31 @@
+﻿using PropertyPro.Core.Contracts;
+using PropertyPro.Infrastructure.Common;
+using PropertyPro.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Core.Services
+{
+    public class TenantService : ITenantService
+    {
+        private readonly IRepository repo;
+        public TenantService(IRepository _repo)
+        {
+                this.repo = _repo;
+        }
+        public async Task CreateTenantAsync(Guid userId)
+        {
+            var tenant = new Tenant()
+            {
+                UserId = userId,
+                Id = Guid.NewGuid()
+            };
+
+            await repo.AddAsync(tenant);
+            await repo.SaveChangesAsync();
+        }
+    }
+}
