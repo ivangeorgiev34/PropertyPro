312fef65bfd1880561275e07cca8a30ab263df41
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-22 14:50:11 +0300

    Add CreatePropertyDto initial commit

---
+++ PropertyPro.Infrastructure/Dtos/Property/CreatePropertyDto.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using PropertyPro.Infrastructure.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace PropertyPro.Infrastructure.Dtos.Property
+{
+    public class CreatePropertyDto
+    {
+        [Required(ErrorMessage = InfrastructureConstants.Property.PROPERTY_TITLE_REQUIRED_ERROR_MESSAGE)]
+        [MaxLength(InfrastructureConstants.Property.PROPERTY_TITLE_MAX_LENGTH_ERROR_MESSAGE)]
+        public string Title { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.Property.PROPERTY_TYPE_REQUIRED_ERROR_MESSAGE)]
+        public string Type { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.Property.PROPERTY_TOWN_REQUIRED_ERROR_MESSAGE)]
+        public string Town { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.Property.PROPERTY_COUNTRY_REQUIRED_ERROR_MESSAGE)]
+        public string Country { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.Property.PROPERTY_MAX_GUESTS_COUNT_REQUIRED_ERROR_MESSAGE)]
+        public int MaxGuestsCount { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.Property.PROPERTY_BEDROOMS_COUNT_REQUIRED_ERROR_MESSAGE)]
+        public int BedroomsCount { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.Property.PROPERTY_BEDS_COUNT_REQUIRED_ERROR_MESSAGE)]
+        public int BedsCount { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.Property.PROPERTY_BATHROOMS_COUNT_REQUIRED_ERROR_MESSAGE)]
+        public int BathroomsCount { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.Property.PROPERTY_FIRST_IMAGE_REQUIRED_ERROR_MESSAGE)]
+        public IFormFile FirstImage { get; set; } = null!;
+
+        public IFormFile? SecondImage { get; set; }
+
+        public IFormFile? ThirdImage { get; set; }
+
+    }
+}
