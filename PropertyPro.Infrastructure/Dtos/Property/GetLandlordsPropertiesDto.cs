d3408ed28f40f77c4aaebfed18c65daafc4644f6
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-22 16:40:23 +0300

    Add GetLandlordsPropertiesDto initial commit

---
+++ PropertyPro.Infrastructure/Dtos/Property/GetLandlordsPropertiesDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Property
+{
+    public class GetLandlordsPropertiesDto
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public string Type { get; set; } = null!;
+
+        public string Town { get; set; } = null!;
+
+        public string Country { get; set; } = null!;
+
+        public int MaxGuestsCount { get; set; }
+
+        public int BedroomsCount { get; set; }
+
+        public int BedsCount { get; set; }
+
+        public int BathroomsCount { get; set; }
+
+        public string FirstImage { get; set; } = null!;
+
+        public string? SecondImage { get; set; }
+
+        public string? ThirdImage { get; set; }
+    }
+}
