1de2feb9d8c4d02d0eac6a5b49255fe29a6c69b7
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-22 14:45:36 +0300

    Add BaseController initial commit

---
+++ PropertyPro/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace PropertyPro.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected string? GetUserId(HttpContext httpContext)
+        {
+            var claimsIdentity = (ClaimsIdentity?)httpContext.User.Identity ?? null;
+            var userIdClaim = claimsIdentity?.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+
+            return userIdClaim?.Value;
+        }
+    }
+}
