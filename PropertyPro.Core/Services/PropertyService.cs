db09afe0d67a4fccea9fbd0b577a5a0134a550ad
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-22 14:52:14 +0300

    Add GetAllPropertiesAsync action to include images and CreatePropertyAsync initial commit

--- PropertyPro.Core/Services/PropertyService.cs
+++ PropertyPro.Core/Services/PropertyService.cs
@@ -15,10 +15,45 @@ namespace PropertyPro.Core.Services
     public class PropertyService : IPropertyService
     {
         private readonly IRepository repo;
-        public PropertyService(IRepository _repo)
+        private readonly ILandlordService landlordService;
+        public PropertyService(IRepository _repo,
+            ILandlordService _landlordService)
         {
             this.repo = _repo;
+            this.landlordService = _landlordService;
         }
+
+        public async Task CreatePropertyAsync(string userId, CreatePropertyDto createPropertyDto, byte[] firstImageBytes, byte[]? secondImageBytes, byte[]? thirdImageBytes)
+        {
+            var landlord = await landlordService.GetLandlordByUserIdAsync(userId);
+
+            if (landlord == null)
+            {
+                throw new InvalidOperationException("Landlord is not found");
+
+            }
+
+            var property = new Property()
+            {
+                LandlordId = landlord.Id,
+                Title = createPropertyDto.Title,
+                Description = createPropertyDto.Description,
+                BathroomsCount = createPropertyDto.BathroomsCount,
+                BedroomsCount = createPropertyDto.BedroomsCount,
+                BedsCount = createPropertyDto.BedsCount,
+                Country = createPropertyDto.Country,
+                FirstImage = firstImageBytes,
+                SecondImage = secondImageBytes,
+                ThirdImage = thirdImageBytes,
+                Type = createPropertyDto.Type,
+                Town = createPropertyDto.Town,
+                MaxGuestsCount = createPropertyDto.MaxGuestsCount
+            };
+
+            await repo.AddAsync(property);
+            await repo.SaveChangesAsync();
+        }
+
         public async Task<List<GetAllPropertiesDto>> GetAllPropertiesAsync()
         {
             var properties = await repo.AllReadonly<Property>()
@@ -33,7 +68,10 @@ namespace PropertyPro.Core.Services
                      Country = p.Country,
                      Description = p.Description,
                      MaxGuestsCount = p.MaxGuestsCount,
-                     Town = p.Town
+                     Town = p.Town,
+                     FirstImage  = Convert.ToBase64String(p.FirstImage),
+                     SecondImage = p.SecondImage == null ? null : Convert.ToBase64String(p.SecondImage),
+                     ThirdImage = p.ThirdImage == null ? null : Convert.ToBase64String(p.ThirdImage)
                  })
                  .ToListAsync();
 
