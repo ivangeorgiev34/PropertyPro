ed053935f60cd26d84d05821e31c8e971b65b968
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-22 14:48:57 +0300

    Add image columns migration

--- PropertyPro.Infrastructure/Migrations/20230621112329_AddImagesToPropertyEntity.Designer.cs
+++ PropertyPro.Infrastructure/Migrations/20230621112329_AddImagesToPropertyEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PropertyPro.Infrastructure.Data;
 
@@ -11,9 +12,10 @@ using PropertyPro.Infrastructure.Data;
 namespace PropertyPro.Infrastructure.Migrations
 {
     [DbContext(typeof(PropertyProDbContext))]
-    partial class PropertyProDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230621112329_AddImagesToPropertyEntity")]
+    partial class AddImagesToPropertyEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
