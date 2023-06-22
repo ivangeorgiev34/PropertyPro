e7449c64df99740138c6258cd35e5fac4cb6985a
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-22 14:46:38 +0300

    Add LandlordService initial commit

---
+++ PropertyPro.Core/Contracts/ILandlordService.cs
@@ -0,0 +1,16 @@
+﻿using PropertyPro.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Core.Contracts
+{
+    public interface ILandlordService
+    {
+        Task<Landlord?> GetLandlordByUserIdAsync(string userId);
+
+        Task CreateLandlordAsync(Guid userId);
+    }
+}
---
+++ PropertyPro.Core/Services/LandlordService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PropertyPro.Core.Contracts;
+using PropertyPro.Infrastructure.Common;
+using PropertyPro.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Core.Services
+{
+    public class LandlordService : ILandlordService
+    {
+        private readonly IRepository repo;
+        public LandlordService(IRepository _repo)
+        {
+                this.repo  =_repo;
+        }
+
+        public async Task CreateLandlordAsync(Guid userId)
+        {
+            var landlord = new Landlord()
+            {
+                Id = Guid.NewGuid(),
+                UserId = userId
+            };
+
+            await repo.AddAsync(landlord);
+            await repo.SaveChangesAsync();
+        }
+
+        public async Task<Landlord?> GetLandlordByUserIdAsync(string userId)
+        {
+            var landlord =await repo.All<Landlord>()
+                .FirstOrDefaultAsync(l=>l.UserId == Guid.Parse(userId));
+
+            var landlords = repo.All<Landlord>().ToList();
+
+            return landlord;
+        }
+    }
+}
