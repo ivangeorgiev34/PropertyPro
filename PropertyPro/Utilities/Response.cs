d8b8adc7c481759c8db9e31f33b53f419aca5c16
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-23 16:56:01 +0300

    Add Response template class initial commit

---
+++ PropertyPro/Utilities/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace PropertyPro.Utilities
+{
+    public class Response
+    {
+        public string Status { get; set; } = null!;
+
+        public string Message { get; set; } = null!;
+
+        public object? Content { get; set; }
+    }
+}
