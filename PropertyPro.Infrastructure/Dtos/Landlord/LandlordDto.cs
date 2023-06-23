fe939b4f7a9b66b31613684215a43870e40bc270
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-23 15:23:56 +0300

    Add LandlordDto initial commit

---
+++ PropertyPro.Infrastructure/Dtos/Landlord/LandlordDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Landlord
+{
+    public class LandlordDto
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string MiddleName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string? Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? PhoneNumber { get; set; }
+    }
+}
