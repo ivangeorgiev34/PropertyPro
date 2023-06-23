68b70307a8cf8a9579e5549fca1484d7c65aac37
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-23 16:55:29 +0300

    Add ApplicationConstants initial commit

---
+++ PropertyPro/Constants/ApplicationConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace PropertyPro.Constants
+{
+    public static class ApplicationConstants
+    {
+        public static class Response
+        {
+            public const string RESPONSE_STATUS_ERROR = "Error";
+            public const string RESPONSE_STATUS_SUCCESS = "Success";
+        }
+    }
+}
