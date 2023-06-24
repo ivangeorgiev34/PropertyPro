582c5760aa65c8c922b9d68b4edc97a9241a0102
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-24 19:38:53 +0300

    Add CreateReviewAsync method to create reviews

--- PropertyPro.Core/Services/ReviewService.cs
+++ PropertyPro.Core/Services/ReviewService.cs
@@ -15,18 +15,53 @@ namespace PropertyPro.Core.Services
     public class ReviewService : IReviewService
     {
         private readonly IRepository repo;
+        private readonly ITenantService tenantService;
 
-        public ReviewService(IRepository _repo)
+        public ReviewService(IRepository _repo,
+            ITenantService _tenantService)
         {
             this.repo = _repo;
+            this.tenantService = _tenantService;
         }
+
+        public async Task<ReviewDto> CreateReviewAsync(CreateReviewDto createReviewDto, string userId, string propertyId)
+        {
+            var tenantDto = await tenantService.GetTenantByUserId(Guid.Parse(userId));
+
+            var review = new Review()
+            {
+                Id = Guid.NewGuid(),
+                Description = createReviewDto.Description,
+                Stars = createReviewDto.Stars,
+                IsActive = true,
+                TenantId = tenantDto!.Id,
+                PropertyId = Guid.Parse(propertyId)
+            };
+
+            await repo.AddAsync(review);
+            await repo.SaveChangesAsync();
+
+
+            var reviewDto = new ReviewDto()
+            {
+                Id = review.Id,
+                Stars = review.Stars,
+                Description = review.Description,
+                Tenant = tenantDto!
+            };
+
+            return reviewDto;
+        }
+
         public async Task<List<ReviewDto>> GetPropertyReviews(string userId, string propertyId)
         {
             var reviews = await repo.All<Review>()
                 .Include(r => r.Property)
                 .ThenInclude(p => p.Landlord)
                 .Include(r => r.Tenant)
-                .Where(r => r.PropertyId == Guid.Parse(propertyId) && r.Property.Landlord.UserId == Guid.Parse(userId))
+                .Where(r => r.PropertyId == Guid.Parse(propertyId)
+                && r.Property.Landlord.UserId == Guid.Parse(userId)
+                && r.IsActive == true)
                 .Select(r => new ReviewDto()
                 {
                     Id = r.Id,
