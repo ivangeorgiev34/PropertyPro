f06b7da9a9b89cc71ed1724d8c8fbaf919cc52d7
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-24 14:36:56 +0300

    Add EditProfileAsync method in the interface IAccountService

--- PropertyPro.Core/Contracts/IAccountService.cs
+++ PropertyPro.Core/Contracts/IAccountService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PropertyPro.Infrastructure.Dtos.Account;
+using PropertyPro.Infrastructure.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,6 @@ namespace PropertyPro.Core.Contracts
 {
     public interface IAccountService
     {
+        Task EditProfileAsync(User user, EditProfileDto editProfileDto);
     }
 }
