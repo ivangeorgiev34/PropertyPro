405c5337f64556d7847222ac0415ce25a3e8dc00
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-24 18:26:42 +0300

    Add TenantDto to transfer data

---
+++ PropertyPro.Infrastructure/Dtos/Tenant/TenantDto.cs
@@ -0,0 +1,25 @@
+﻿using PropertyPro.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Tenant
+{
+    public class TenantDto
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string MiddleName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string? Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public string? ProfilePicture { get; set; }
+    }
+}
