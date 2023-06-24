f3b8b0b1a9d2def82d2aef068b77233e45b877da
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-24 19:37:22 +0300

    Add GetTenantByUserId in interface ITenantService

--- PropertyPro.Core/Contracts/ITenantService.cs
+++ PropertyPro.Core/Contracts/ITenantService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyPro.Infrastructure.Dtos.Tenant;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,7 @@ namespace PropertyPro.Core.Contracts
     public interface ITenantService
     {
         Task CreateTenantAsync(Guid userId);
+
+        Task<TenantDto?> GetTenantByUserId(Guid userId);
     }
 }
