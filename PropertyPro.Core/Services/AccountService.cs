de51652fdc730f4f197c6af84b7a29bbc9059a86
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-24 13:55:45 +0300

    Add AccountService and IAccountService interface initial commit

---
+++ PropertyPro.Core/Services/AccountService.cs
@@ -0,0 +1,13 @@
+﻿using PropertyPro.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Core.Services
+{
+    public class AccountService : IAccountService
+    {
+    }
+}
