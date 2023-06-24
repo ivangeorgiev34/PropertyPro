f8c9fff1e4a92db062ea31a0a7fc3b7e248d0571
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-24 19:38:13 +0300

    Add CreateReviewAsync in interface IReviewService

--- PropertyPro.Core/Contracts/IReviewService.cs
+++ PropertyPro.Core/Contracts/IReviewService.cs
@@ -10,5 +10,7 @@ namespace PropertyPro.Core.Contracts
     public interface IReviewService
     {
         Task<List<ReviewDto>> GetPropertyReviews(string userId,string propertyId);
+
+        Task<ReviewDto> CreateReviewAsync(CreateReviewDto createReviewDto,string userId, string propertyId);
     }
 }
