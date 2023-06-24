fc63083048290d49a9146e88d14278fb840bec34
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-24 19:36:24 +0300

    Add CreateReview method to create reviews

--- PropertyPro/Controllers/ReviewController.cs
+++ PropertyPro/Controllers/ReviewController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
+using PropertyPro.Constants;
+using PropertyPro.Core.Contracts;
+using PropertyPro.Infrastructure.Dtos.Review;
+using PropertyPro.Utilities;
 
 namespace PropertyPro.Controllers
 {
@@ -7,6 +14,44 @@ namespace PropertyPro.Controllers
     [ApiController]
     public class ReviewController : BaseController
     {
+        private readonly IPropertyService propertyService;
+        private readonly IReviewService reviewService;
+        public ReviewController(IPropertyService _propertyService,
+            IReviewService _reviewService)
+        {
+            this.propertyService = _propertyService;
+            this.reviewService = _reviewService;
+        }
 
+        [HttpPost]
+        [Route("create/{propertyId}")]
+        [Authorize(Roles = "Tenant", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> CreateReview(CreateReviewDto createReviewDto, string propertyId)
+        {
+            if (IsIdValidGuidAndNotNull(propertyId) == false
+                || await propertyService.PropertyExistsAsync(propertyId) == false)
+            {
+                return BadRequest(new Response()
+                {
+                    Status = ApplicationConstants.Response.RESPONSE_STATUS_ERROR,
+                    Message = "Property doesn't exist"
+                });
+            }
+
+            var userId = GetUserId(HttpContext);
+
+            var review = await reviewService.CreateReviewAsync(createReviewDto, userId!,propertyId);
+
+            return Ok(new Response()
+            {
+                Status = ApplicationConstants.Response.RESPONSE_STATUS_ERROR,
+                Message = "Review created successfully",
+                Content = new
+                {
+                    Review = review
+                }
+            });
+
+        }
     }
 }
