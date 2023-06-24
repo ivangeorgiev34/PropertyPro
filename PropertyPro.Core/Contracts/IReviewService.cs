f8bb276965760c04c7ee91f29d041b516601a9bf
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-24 18:25:05 +0300

    Add ReviewService

---
+++ PropertyPro.Core/Contracts/IReviewService.cs
@@ -0,0 +1,14 @@
+﻿using PropertyPro.Infrastructure.Dtos.Review;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Core.Contracts
+{
+    public interface IReviewService
+    {
+        Task<List<ReviewDto>> GetPropertyReviews(string userId,string propertyId);
+    }
+}
---
+++ PropertyPro.Infrastructure/Dtos/Review/ReviewDto.cs
@@ -0,0 +1,20 @@
+﻿using PropertyPro.Infrastructure.Dtos.Tenant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Review
+{
+    public class ReviewDto
+    {
+        public Guid Id { get; set; }
+
+        public double Stars { get; set; }
+
+        public string? Description { get; set; }
+
+        public TenantDto Tenant { get; set; } = null!;
+    }
+}
