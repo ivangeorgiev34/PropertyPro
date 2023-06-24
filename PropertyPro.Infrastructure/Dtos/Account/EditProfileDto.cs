cb8dee51e3240a0e37d272ce78d35750b1faa92b
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-24 14:33:43 +0300

    Add EditProfileDto for data transferation in EditProfile action initial commit

---
+++ PropertyPro.Infrastructure/Dtos/Account/EditProfileDto.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using PropertyPro.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Account
+{
+    public class EditProfileDto
+    { 
+
+        [Required(ErrorMessage = InfrastructureConstants.User.FIRST_NAME_REQUIRED_ERROR_MESSAGE)]
+        public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.User.MIDDLE_NAME_REQUIRED_ERROR_MESSAGE)]
+        public string MiddleName { get; set; } = null!;
+
+        [Required(ErrorMessage = InfrastructureConstants.User.LAST_NAME_REQUIRED_ERROR_MESSAGE)]
+        public string LastName { get; set; } = null!;
+
+        public string? Gender { get; set; }
+
+        [Required(ErrorMessage = InfrastructureConstants.User.AGE_REQUIRED_ERROR_MESSAGE)]
+        [Range(InfrastructureConstants.User.AGE_MIN_VALUE, InfrastructureConstants.User.AGE_MAX_VALUE, ErrorMessage = InfrastructureConstants.User.AGE_REQUIRED_ERROR_MESSAGE)]
+        public int Age { get; set; }
+
+        public IFormFile? ProfilePicture { get; set; }
+
+    }
+}
