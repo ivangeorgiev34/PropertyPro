f16db6588bac9bda9c8454272b31ff9a28dc4332
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-24 18:49:07 +0300

    Add CreateReviewDto to transfer data

---
+++ PropertyPro.Infrastructure/Dtos/Review/CreateReviewDto.cs
@@ -0,0 +1,20 @@
+﻿using PropertyPro.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Review
+{
+    public class CreateReviewDto
+    {
+
+        [Required(ErrorMessage = InfrastructureConstants.Review.REVIEW_STARS_REQUIRED_ERROR_MESSAGE)]
+        [Range(InfrastructureConstants.Review.REVIEW_STARS_MIN_VALUE_ERROR_MESSAGE, InfrastructureConstants.Review.REVIEW_STARS_MAX_VALUE_ERROR_MESSAGE)]
+        public double Stars { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
