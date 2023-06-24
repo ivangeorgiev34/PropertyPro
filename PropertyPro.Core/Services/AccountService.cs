be501869391ef0ef027b600196ad521fcd1ceaef
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-24 14:36:29 +0300

    Add EditProfileAsync method for editing users' information

--- PropertyPro.Core/Services/AccountService.cs
+++ PropertyPro.Core/Services/AccountService.cs
@@ -1,13 +1,44 @@
 ﻿using PropertyPro.Core.Contracts;
+using PropertyPro.Infrastructure.Common;
+using PropertyPro.Infrastructure.Dtos.Account;
+using PropertyPro.Infrastructure.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PropertyPro.Core.Services
 {
     public class AccountService : IAccountService
     {
+        private IRepository repo;
+        public AccountService(IRepository _repo)
+        {
+            this.repo = _repo;
+        }
+        public async Task EditProfileAsync(User user, EditProfileDto editProfileDto)
+        {
+            byte[]? imageBytes = null;
+
+            if (editProfileDto.ProfilePicture != null)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    await editProfileDto.ProfilePicture.CopyToAsync(ms);
+                    imageBytes = ms.ToArray();
+                }
+            }
+
+            user.FirstName = editProfileDto.FirstName;
+            user.MiddleName = editProfileDto.MiddleName;
+            user.LastName = editProfileDto.LastName;
+            user.Age = editProfileDto.Age;
+            user.Gender = editProfileDto.Gender;
+            user.ProfilePicture = imageBytes;
+
+            await repo.SaveChangesAsync();
+        }
     }
 }
