3fbcd2086b6b57e0b3e3fec0acfaf2389a0c6ba2
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-26 18:22:38 +0300

    Add BookingDto to transfer data

---
+++ PropertyPro.Infrastructure/Dtos/Booking/BookingDto.cs
@@ -0,0 +1,27 @@
+﻿using PropertyPro.Infrastructure.Constants;
+using PropertyPro.Infrastructure.Dtos.Property;
+using PropertyPro.Infrastructure.Dtos.Tenant;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Booking
+{
+    public class BookingDto
+    {
+        public Guid Id { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int Guests { get; set; }
+
+        public TenantDto Tenant { get; set; } = null!;
+
+        public PropertyDto Property { get; set; } = null!;
+    }
+}
