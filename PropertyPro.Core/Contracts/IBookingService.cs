3ac96f206934622ecf45db22c3c48e21f947a023
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-27 11:56:39 +0300

    Add CreateBookingAsync method to create bookings

---
+++ PropertyPro.Core/Contracts/IBookingService.cs
@@ -0,0 +1,14 @@
+﻿using PropertyPro.Infrastructure.Dtos.Booking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Core.Contracts
+{
+    public interface IBookingService
+    {
+        Task<BookingDto> CreateBookingAsync(CreateBookingDto createBookingDto,string userId,string propertyId, DateTime startDate, DateTime endDate);
+    }
+}
