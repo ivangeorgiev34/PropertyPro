2b25d763ba648b4f07bdf754d9077de90845d612
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-28 22:04:27 +0300

    Add EditBooking action to edit a user's booking

--- PropertyPro/Controllers/BookingController.cs
+++ PropertyPro/Controllers/BookingController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using PropertyPro.Constants;
 using PropertyPro.Core.Contracts;
 using PropertyPro.Infrastructure.Dtos.Booking;
@@ -88,6 +89,87 @@ namespace PropertyPro.Controllers
             {
                 Status = ApplicationConstants.Response.RESPONSE_STATUS_SUCCESS,
                 Message = "Booking created successfully",
+                Content = new
+                {
+                    Booking = bookingDto
+                }
+            });
+        }
+
+        [HttpPut]
+        [Route("edit/{bookingId}")]
+        [Authorize(Roles = "Tenant",AuthenticationSchemes =JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> EditBooking(EditBookingDto editBookingDto,string bookingId)
+        {
+            if (IsIdValidGuidAndNotNull(bookingId) == false)
+            {
+                return BadRequest(new Response()
+                {
+                    Status = ApplicationConstants.Response.RESPONSE_STATUS_ERROR,
+                    Message = "Property doesn't exist"
+                });
+            }
+
+            var convertFirstDateResult = DateTime.TryParseExact(DateTime.Parse(editBookingDto.StartDate).ToString("dd/MM/yyyy"), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime startDate);
+            var convertSecondDateResult = DateTime.TryParseExact(DateTime.Parse(editBookingDto.EndDate).ToString("dd/MM/yyyy"), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime endDate);
+
+            if (convertFirstDateResult == false
+                && convertSecondDateResult == false)
+            {
+                return BadRequest(new Response()
+                {
+                    Status = ApplicationConstants.Response.RESPONSE_STATUS_ERROR,
+                    Message = "Invalid dates"
+                });
+            }
+
+            if (DateTime.Compare(startDate, endDate) >= 0)
+            {
+                return BadRequest(new Response()
+                {
+                    Status = ApplicationConstants.Response.RESPONSE_STATUS_ERROR,
+                    Message = "Start date cannot be later than end date"
+                });
+            }
+
+            try
+            {
+                //if CanBookingBeBooked is true, then booking cannot be booked, otherwise booking can be booked
+                if (await bookingService.CanBookingBeBooked(startDate, endDate) == true)
+                {
+                    return BadRequest(new Response()
+                    {
+                        Status = ApplicationConstants.Response.RESPONSE_STATUS_ERROR,
+                        Message = "The dates you chose are busy"
+                    });
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                return BadRequest(new Response()
+                {
+                    Status = ApplicationConstants.Response.RESPONSE_STATUS_ERROR,
+                    Message = e.Message
+                });
+            }
+
+            var userId = GetUserId(HttpContext);
+
+            if (userId == null)
+            {
+                return StatusCode(500, new Response()
+                {
+                    Status = ApplicationConstants.Response.RESPONSE_STATUS_ERROR,
+                    Message = "User is not logged in"
+                });
+            }
+
+            var bookingDto = await bookingService.EditBookingAsync(editBookingDto, bookingId, userId, startDate,endDate);
+
+            return StatusCode(200, new Response()
+            {
+                Status = ApplicationConstants.Response.RESPONSE_STATUS_SUCCESS,
+                Message = "Booking edited successfully",
                 Content = new
                 {
                     Booking = bookingDto
