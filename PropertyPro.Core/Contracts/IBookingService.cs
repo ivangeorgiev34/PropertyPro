cdbaab046118ce60420ec663a4bbcb3d7696abe5
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-28 22:03:19 +0300

    Add EditBookingAsync method to edit existing bookings made by the user that has requested to edit it

--- PropertyPro.Core/Contracts/IBookingService.cs
+++ PropertyPro.Core/Contracts/IBookingService.cs
@@ -1,4 +1,5 @@
 ﻿using PropertyPro.Infrastructure.Dtos.Booking;
+using PropertyPro.Infrastructure.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,7 @@ namespace PropertyPro.Core.Contracts
         Task<BookingDto> CreateBookingAsync(CreateBookingDto createBookingDto,string userId,string propertyId, DateTime startDate, DateTime endDate);
 
         Task<bool> CanBookingBeBooked(DateTime startDate, DateTime endDate);
+
+        Task<BookingDto> EditBookingAsync(EditBookingDto editBookingDto,string bookingId,string userId, DateTime startDate, DateTime endDate);
     }
 }
