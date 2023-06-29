86b538c633ed3c2ba8339dc3e02fdfbfec34f30d
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-06-29 12:16:01 +0300

    Add DeleteBookingAsync method to delete existing bookings by given id

--- PropertyPro.Core/Contracts/IBookingService.cs
+++ PropertyPro.Core/Contracts/IBookingService.cs
@@ -10,14 +10,16 @@ namespace PropertyPro.Core.Contracts
 {
     public interface IBookingService
     {
-        Task<BookingDto> CreateBookingAsync(CreateBookingDto createBookingDto,string userId,string propertyId, DateTime startDate, DateTime endDate);
+        Task<BookingDto> CreateBookingAsync(CreateBookingDto createBookingDto, string userId, string propertyId, DateTime startDate, DateTime endDate);
 
         Task<bool> CanBookingBeBooked(DateTime startDate, DateTime endDate);
 
-        Task<BookingDto> EditBookingAsync(EditBookingDto editBookingDto,string bookingId,string userId, DateTime startDate, DateTime endDate);
+        Task<BookingDto> EditBookingAsync(EditBookingDto editBookingDto, string bookingId, string userId, DateTime startDate, DateTime endDate);
 
         Task<bool> BookingExistsByIdAsync(string bookingId);
 
-        Task<Booking?> GetBookingByIdAsync(string bookingId);    
+        Task<Booking?> GetBookingByIdAsync(string bookingId);
+
+        Task DeleteBookingAsync(string bookingId, string userId);
     }
 }
