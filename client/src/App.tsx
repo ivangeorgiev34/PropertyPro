a4733ea77b57d30c6d370c13c3f1aa1508b01b05
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-04 22:36:06 +0300

    Add routes and wrapper for the app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { Navigation } from './components/layout/navigation/Navigation';
+import { Login } from './pages/Login/Login';
 
 function App() {
   return (
-    <Navigation></Navigation>
-      // <Routes>
-      //   <Route path='/home' element={<Home/>}/>
-      // </Routes>
+    <React.Fragment>
+      <Navigation></Navigation>
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </React.Fragment>
   );
 }
 
