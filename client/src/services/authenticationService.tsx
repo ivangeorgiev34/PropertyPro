60bb3b85cf885a37f58a656e81d7d6abd8757443
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-04 22:44:07 +0300

    Add userLogin function for user login funtionalty

--- client/src/services/authenticationService.tsx
+++ client/src/services/authenticationService.tsx
@@ -1,3 +1,25 @@
-export async function login() {
+import { BASE_URL } from "../constants/constants";
 
+export async function userLogin(email: string, password: string) {
+    try {
+        const response = await fetch(`${BASE_URL}/account/login`, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        });
+
+        const userJson = await response.json();
+
+        return userJson;
+
+    } catch (error) {
+        console.log(error);
+
+        return error;
+    }
 }
