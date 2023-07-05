a8226a0772033a9c31785f4f330bbc0637eb7cee
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-05 18:27:45 +0300

    Add role property to context

--- client/src/interfaces/IAuthState.tsx
+++ client/src/interfaces/IAuthState.tsx
@@ -1,12 +1,13 @@
-export default interface IAuthState{
-    id?:string | null,
-    firstName?:string| null,
-    middleName?:string| null,
-    lastName?:string| null,
-    email?:string| null,
-    gender?:string| null,
-    profilePicture?:string| null,
-    phoneNumber?:string| null,
-    age?:number | null,
-    token?:string| null
+export default interface IAuthState {
+    id?: string | null,
+    firstName?: string | null,
+    middleName?: string | null,
+    lastName?: string | null,
+    email?: string | null,
+    gender?: string | null,
+    profilePicture?: string | null,
+    phoneNumber?: string | null,
+    age?: number | null,
+    role: string | null,
+    token?: string | null
 }
