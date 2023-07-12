011e4c82ea31c8973f15c49e4d406e527e763fa6
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-13 02:46:04 +0300

    Add email and password validations

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,13 +1,14 @@
 import React, { FormEventHandler, FormHTMLAttributes, useState } from "react";
 import styles from "./Login.module.scss"
 import { Link } from "react-router-dom";
-import { userLogin } from "../../services/authenticationService";
+import { userLogin, userRegister } from "../../services/authenticationService";
 import { useForm } from "../../hooks/useForm";
 import { login } from "../../store/auth";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import { useNavigate } from "react-router-dom";
 import { toggleLoaderOff, toggleLoaderOn } from "../../store/loader"
 import ILoginForm from "../../interfaces/ILoginForm";
+import { useError } from "../../hooks/useError";
 
 
 export const Login: React.FC = () => {
@@ -18,6 +19,11 @@ export const Login: React.FC = () => {
     const dispatch = useAppDispatch()
 
     const { formValues, onFormChange } = useForm<ILoginForm>({
+        email: "",
+        password: ""
+    });
+
+    const { formErrors, onFormErrorChange } = useError<ILoginForm>({
         email: "",
         password: ""
     });
@@ -72,28 +78,79 @@ export const Login: React.FC = () => {
             });
     }
 
+    const emailValidation = (e: React.FormEvent<HTMLInputElement>): void => {
+
+        if (formValues.email.length === 0) {
+            onFormErrorChange(e, "Email is required");
+        } else if (new RegExp('^[A-Za-z0-9_\.]+@[A-Za-z]+\.[A-Za-z]{2,3}$').test(formValues.email) === false) {
+            onFormErrorChange(e, "Invalid email");
+        } else {
+
+            onFormErrorChange(e, "");
+
+        }
+    };
+
+    const passwordValidation = (e: React.FormEvent<HTMLInputElement>): void => {
+
+        if (formValues.password.length === 0) {
+
+            onFormErrorChange(e, "Password is required");
+
+        } else if (formValues.password.length < 6) {
+
+            onFormErrorChange(e, "Password must be at least 6 symbols");
+
+        } else {
+
+            onFormErrorChange(e, "");
+
+        }
+    };
+
+
+    const areFormValuesIncorrect = (): boolean => {
+        for (let key in formErrors) {
+            if (formErrors.hasOwnProperty(key) && (formErrors as Record<string, any>)[key] !== '') {
+                return true;
+            }
+        }
+
+        for (let key in formValues) {
+            if (formValues.hasOwnProperty(key) && (formValues as Record<string, any>)[key] === '') {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
     return (
         <div className={styles.formWrapper}>
             <form className={styles.loginForm} onSubmit={onLogInFormSubmit}>
                 <h2 className={styles.loginHeading}>Sign in to PropertyPro</h2>
                 <div className={styles.emailContainer}>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" placeholder="Email..." value={formValues.email} onChange={onFormChange} />
+                    <input type="email" name="email" placeholder="Email..." value={formValues.email} onChange={onFormChange}
+                        onBlur={emailValidation} />
+                    {<p className={styles.error}>{formErrors.email}</p>}
                 </div>
                 <div className={styles.passwordContainer}>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" placeholder="Password..." value={formValues.password} onChange={onFormChange} />
+                    <input type="password" name="password" placeholder="Password..." value={formValues.password} onChange={onFormChange}
+                        onBlur={passwordValidation} />
+                    {<p className={styles.error}>{formErrors.password}</p>}
                 </div>
                 <div className={styles.errorsContainer}>
                     {errors.map(e => {
-                        return <span key={e} className={styles.error}>{e}</span>
+                        return <p key={e} className={styles.error}>{e}</p>
                     })}
                 </div>
-                <span className={styles.registerLinks}>
+                <span className={styles.registerLink}>
                     Don't have an account?
                     <Link to={"/register"}> Click here to register!</Link>
                 </span>
-                <button type="submit" className={styles.loginBtn}>Log In</button>
+                <button type="submit" disabled={areFormValuesIncorrect()} className={styles.loginBtn}>Log In</button>
             </form>
         </div >
     );
