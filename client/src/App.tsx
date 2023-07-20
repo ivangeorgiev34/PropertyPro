65e404c2e319e198c1a4561d3024d0285b2b743f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-20 20:57:26 +0300

    Add PrivateRouteGuard and PublicRouteGuard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Routes, Route, Outlet } from 'react-router-dom';
 
@@ -11,22 +11,35 @@ import { MyBookings } from './pages/MyBookings/MyBookings';
 import { Unauthorized } from './pages/Unauthorized/Unauthorized';
 import { Loader } from './components/loader/Loader';
 import { useAppSelector } from './hooks/reduxHooks';
+import { PrivateRouteGuard } from './guards/PrivateRouteGuard';
+import { PublicRouteGuard } from "./guards/PublicRouteGuard";
+import { ProfileInformation } from './pages/ProfileInformation/ProfileInformation';
+import { ProfileEdit } from './pages/ProfileEdit/ProfileEdit';
 
 function App() {
   const { isLoading } = useAppSelector((state) => state.loader)
+  const { id } = useAppSelector((state) => state.auth);
 
   return (
     <React.Fragment>
-      {isLoading === true ? <Loader /> : null}
+      {isLoading === false
+        ? null
+        : <Loader />}
       <Navigation></Navigation>
       <Routes>
+        <Route path='/unauthorized' element={<Unauthorized />} />
         <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/my-properties' element={<MyProperties />} />
-        <Route path='/my-bookings' element={<MyBookings />} />
-        <Route path='/unauthorized' element={<Unauthorized />} />
+        <Route element={<PublicRouteGuard />}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Route>
+        <Route element={<PrivateRouteGuard />}>
+          <Route path="/profile/edit/:userId" element={<ProfileEdit />} />
+          <Route path="/profile/:userId" element={<ProfileInformation />} />
+          <Route path='/my-properties' element={<MyProperties />} />
+          <Route path='/my-bookings' element={<MyBookings />} />
+        </Route>
       </Routes>
     </React.Fragment>
   );
