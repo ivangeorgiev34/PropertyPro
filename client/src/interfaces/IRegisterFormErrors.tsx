4bea0ada06f88fe0c4818eb5954f77a6e88ba6a4
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-21 20:52:27 +0300

    Add IRegisterFormErrors interface

---
+++ client/src/interfaces/IRegisterFormErrors.tsx
@@ -0,0 +1,12 @@
+export interface IRegisterFormErrors {
+    firstName: string,
+    middleName: string,
+    lastName: string,
+    gender: string,
+    age: string,
+    email: string,
+    username: string,
+    phoneNumber: string,
+    password: string,
+    confirmPassword: string
+};
