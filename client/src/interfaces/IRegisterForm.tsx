d3d85e624872ef66e88bf8fba8d26a4a64c5036f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-21 20:52:45 +0300

    Add IRegisterForm interface

---
+++ client/src/interfaces/IRegisterForm.tsx
@@ -0,0 +1,12 @@
+export default interface IRegisterForm {
+    firstName: string,
+    middleName: string,
+    lastName: string,
+    gender: string,
+    age: number,
+    email: string,
+    username: string,
+    phoneNumber: string,
+    password: string,
+    confirmPassword: string
+}
