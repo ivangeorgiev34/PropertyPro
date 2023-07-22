7145bd0d7ab33c37cc5509f7b9f0b529b0a34772
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-22 21:07:02 +0300

    Change routes to be children of Loader component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,6 +15,7 @@ import { PrivateRouteGuard } from './guards/PrivateRouteGuard';
 import { PublicRouteGuard } from "./guards/PublicRouteGuard";
 import { ProfileInformation } from './pages/ProfileInformation/ProfileInformation';
 import { ProfileEdit } from './pages/ProfileEdit/ProfileEdit';
+import { Footer } from './components/layout/footer/Footer';
 
 function App() {
   const { isLoading } = useAppSelector((state) => state.loader)
@@ -23,24 +24,44 @@ function App() {
   return (
     <React.Fragment>
       {isLoading === false
-        ? null
-        : <Loader />}
-      <Navigation></Navigation>
-      <Routes>
-        <Route path='/unauthorized' element={<Unauthorized />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route element={<PublicRouteGuard />}>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-        </Route>
-        <Route element={<PrivateRouteGuard />}>
-          <Route path="/profile/edit/:userId" element={<ProfileEdit />} />
-          <Route path="/profile/:userId" element={<ProfileInformation />} />
-          <Route path='/my-properties' element={<MyProperties />} />
-          <Route path='/my-bookings' element={<MyBookings />} />
-        </Route>
-      </Routes>
+        ? <React.Fragment>
+          <Navigation />
+          <Routes>
+            <Route path='/unauthorized' element={<Unauthorized />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route element={<PublicRouteGuard />}>
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+            </Route>
+            <Route element={<PrivateRouteGuard />}>
+              <Route path="/profile/edit/:userId" element={<ProfileEdit />} />
+              <Route path="/profile/:userId" element={<ProfileInformation />} />
+              <Route path='/my-properties' element={<MyProperties />} />
+              <Route path='/my-bookings' element={<MyBookings />} />
+            </Route>
+          </Routes>
+          <Footer />
+        </React.Fragment>
+        : <Loader>
+          <Navigation />
+          <Routes>
+            <Route path='/unauthorized' element={<Unauthorized />} />
+            <Route path='/' element={<Home />} />
+            { }
+            <Route element={<PublicRouteGuard />}>
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+            </Route>
+            <Route element={<PrivateRouteGuard />}>
+              <Route path="/profile/edit/:userId" element={<ProfileEdit />} />
+              <Route path="/profile/:userId" element={<ProfileInformation />} />
+              <Route path='/my-properties' element={<MyProperties />} />
+              <Route path='/my-bookings' element={<MyBookings />} />
+            </Route>
+          </Routes>
+          <Footer />
+        </Loader>}
     </React.Fragment>
   );
 }
