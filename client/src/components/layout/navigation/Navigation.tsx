215ecfa80bdb12dc97e4d0a73d2132d78bef5c9b
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-23 21:23:28 +0300

    Add register, login, logout buttons and role check

--- client/src/components/layout/navigation/Navigation.tsx
+++ client/src/components/layout/navigation/Navigation.tsx
@@ -7,7 +7,7 @@ import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
 export const Navigation: React.FC = () => {
 
     const { token, profilePicture, id, role, firstName, lastName } = useAppSelector((state) => state.auth)
-    const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch();
 
     const [hasProfilePicture, setHasProfilePicture] = useState(false);
 
@@ -20,43 +20,50 @@ export const Navigation: React.FC = () => {
     }, []);
 
     return (
-        <nav id={styles.navBar}>
-            <div className={styles.menu}>
-                <Link to={"/home"}>
-                    <img className={styles.navLogoPicture} src="clipart-house-logo-6.png" alt="propertypro-logo" />
-                </Link>
-                <ul>
-                    {role === "Tenant"
-                        ? <li className={styles.navBarItem}><Link to={"/my-bookings"}>My bookings</Link></li>
-                        : role === "Landlord"
-                            ? <li className={styles.navBarItem}><Link to={"/my-properties"}>My properties</Link></li>
+        <header>
+            <nav id={styles.navBar}>
+                <div className={styles.menu}>
+                    <Link to={"/home"} className={styles.logo}>
+                        <img className={styles.navLogoPicture} src="clipart-house-logo-6.png" alt="propertypro-logo" />
+                        PropertyPro
+                    </Link>
+                    <ul>
+                        {role !== null
+                            ? role === "Tenant"
+                                ? <li className={styles.navBarItem}><Link className={styles.myBookings} to={"/my-bookings"}>My bookings</Link></li>
+                                : role === "Landlord"
+                                    ? <li className={styles.navBarItem}><Link className={styles.myProperties} to={"/my-properties"}>My properties</Link></li>
+                                    : null
                             : null}
+                    </ul>
+                </div>
+                <ul>
+                    {token === null
+                        ? <React.Fragment>
+                            <li className={styles.navBarItem}><Link className={styles.loginBtn} to={"/login"} >Login</Link></li>
+                            <li className={styles.navBarItem}><Link className={styles.registerBtn} to={"/register"} >Register</Link></li>
+                        </React.Fragment>
+                        : <React.Fragment>
+                            <li className={`${styles.navBarItem} ${styles.greeting}`}>Hello, {firstName} {lastName}!</li>
+                            <li className={`${styles.navBarItem} ${styles.navBarItem}`}>
+                                <Link to={`/profile/${id}`}>
+                                    <img className={styles.profilePicture} src={
+                                        profilePicture === null
+                                            ? "https://thumbs.dreamstime.com/b/user-icon-flat-style-isolated-grey-background-user-icon-flat-style-isolated-grey-background-your-design-logo-131213475.jpg"
+                                            : `data:image/png;base64,${profilePicture!}`
+                                    } alt="profile picture" />
+                                </Link>
+                            </li>
+                            <li className={styles.navBarItem}>
+                                <Link className={styles.logoutBtn} onClick={() => dispatch(logout())} to={"/home"}>
+                                    Logout
+                                </Link>
+                            </li>
+                        </React.Fragment>
+                    }
                 </ul>
-            </div>
-            <ul>
-                {token === null
-                    ? <React.Fragment>
-                        <li className={styles.navBarItem}><Link to={"/login"} >Login</Link></li>
-                        <li className={styles.navBarItem}><Link to={"/register"} >Register</Link></li>
-                    </React.Fragment>
-                    : <React.Fragment>
-                        <li className={styles.navBarItem}>Hello, {firstName} {lastName}!</li>
-                        <li className={`${styles.navBarItem} ${styles.navBarItem}`}>
-                            <Link to={`/profile/${id}`}>
-                                <img className={styles.profilePicture} src={
-                                    profilePicture === null
-                                        ? "https://thumbs.dreamstime.com/b/user-icon-flat-style-isolated-grey-background-user-icon-flat-style-isolated-grey-background-your-design-logo-131213475.jpg"
-                                        : `data:image/png;base64,${profilePicture!}`
-                                } alt="profile picture" />
-                            </Link>
-                        </li>
-                        <li className={styles.navBarItem}>
-                            <button className={styles.logoutBtn} onClick={() => dispatch(logout())}>Logout</button>
-                        </li>
-                    </React.Fragment>
-                }
-            </ul>
-        </nav>
+            </nav>
+        </header>
     );
 
 }
