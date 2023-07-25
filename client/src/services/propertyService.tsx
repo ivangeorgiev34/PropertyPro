587271b6e4a4f6da185080441c877094f6067ee7
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-25 22:49:13 +0300

    Add getLandlordPropertyById method

--- client/src/services/propertyService.tsx
+++ client/src/services/propertyService.tsx
@@ -17,3 +17,21 @@ export async function getLandlordsProperties(userId: string, token: string) {
         return error;
     }
 }
+
+export async function getLandlordPropertyById(userId: string, propertyId: string, token: string) {
+    try {
+        const response = await fetch(`${BASE_URL}/property/properties/${userId}/${propertyId}`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        const propertiesJson = await response.json();
+
+        return propertiesJson;
+
+    } catch (error) {
+        return error;
+    }
+}
