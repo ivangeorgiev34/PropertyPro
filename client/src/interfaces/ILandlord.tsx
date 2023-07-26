2cc706b9b55d3a21436fd0f77b687013f1b8dd68
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-26 19:14:04 +0300

    Add ILandlord interface

---
+++ client/src/interfaces/ILandlord.tsx
@@ -0,0 +1,11 @@
+export default interface ILandlord {
+    id: string,
+    email: string,
+    age: number,
+    firstName: string,
+    middleName: string,
+    lastName: string,
+    gender: string | null,
+    phoneNumber: string,
+    username: string
+}
