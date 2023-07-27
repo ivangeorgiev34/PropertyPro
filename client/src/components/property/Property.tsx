08410e620382c46df1015f2e8a9a9939f9f4770c
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-27 16:22:44 +0300

    Change componeny name

--- client/src/components/property/Property.tsx
+++ client/src/components/property/Property.tsx
@@ -3,15 +3,17 @@ import IProperty from "../../interfaces/IProperty";
 import styles from "./Property.module.scss";
 import { Link } from "react-router-dom";
 
-export const MyProperty: React.FC<IProperty> = (props) => {
+export const Property: React.FC<IProperty> = (props) => {
     return (
         <div className={styles.myPropertyCard}>
-            <img className={styles.myPropertyCardImage}
-                src={`data:image/png;base64,${props.firstImage}`}
-                alt="" />
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
-            <p>{props.type} in {props.town}, {props.country}</p>
+            <div className={styles.myPropertyCardContent}>
+                <img className={styles.myPropertyCardImage}
+                    src={`data:image/png;base64,${props.firstImage}`}
+                    alt="" />
+                <h2>{props.title}</h2>
+                <p>{props.description}</p>
+                <p>{props.type} in {props.town}, {props.country}</p>
+            </div>
             <Link to={`/property/details/${props.id}`} className={styles.propertyDetails}>Details</Link>
         </div>
     );
