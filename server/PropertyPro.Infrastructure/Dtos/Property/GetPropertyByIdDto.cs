8015400e728f29efc4b48ebc6df0918b9e08af32
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-27 22:21:20 +0300

    Add GetPropertyByIdDto for transfering data

---
+++ server/PropertyPro.Infrastructure/Dtos/Property/GetPropertyByIdDto.cs
@@ -0,0 +1,46 @@
+﻿using PropertyPro.Infrastructure.Dtos.Landlord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Property
+{
+	public class GetPropertyByIdDto
+	{
+		public Guid Id { get; set; }
+
+		public string Title { get; set; } = null!;
+
+		public string? Description { get; set; }
+
+		public string Type { get; set; } = null!;
+
+		public string Town { get; set; } = null!;
+
+		public string Country { get; set; } = null!;
+
+		public decimal GuestPricePerNight { get; set; }
+
+		public int MaxGuestsCount { get; set; }
+
+		public int BedroomsCount { get; set; }
+
+		public int BedsCount { get; set; }
+
+		public int BathroomsCount { get; set; }
+
+		public double AverageRating { get; set; }
+
+		public int ReviewsCount { get; set; }
+
+		public string FirstImage { get; set; } = null!;
+
+		public string? SecondImage { get; set; }
+
+		public string? ThirdImage { get; set; }
+
+		public LandlordDto Landlord { get; set; } = null!;
+	}
+}
