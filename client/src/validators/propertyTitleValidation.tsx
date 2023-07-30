4f5f64bac114d8c1d922caae84c4f411a06e9ffd
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-30 10:35:54 +0300

    Change propertyTitleValidation file location

--- client/src/validators/propertyTitleValidation.tsx
+++
@@ -1,8 +0,0 @@
-export const propertyTitleValidation = (title: string): string => {
-    console.log(title);
-    return title.length === 0
-        ? "Title is required"
-        : title.length > 80
-            ? "Title cannot be more than 80 symbols"
-            : "";
-};
