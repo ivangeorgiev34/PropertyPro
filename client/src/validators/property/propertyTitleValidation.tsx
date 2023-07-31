063b14795f7d0365992085b73a95050eb5b423a0
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-07-31 18:50:47 +0300

    Remove unneccesary console log

--- client/src/validators/property/propertyTitleValidation.tsx
+++ client/src/validators/property/propertyTitleValidation.tsx
@@ -1,5 +1,4 @@
 export const propertyTitleValidation = (title: string): string => {
-    console.log(title);
     return title.length === 0
         ? "Title is required"
         : title.length > 80
