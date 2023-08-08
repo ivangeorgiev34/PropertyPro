be04d9a4b8caa417184cd953f6510de5325571d6
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-08 21:10:48 +0300

    Add LandlordHome page component

---
+++ client/src/components/home/GuestHome/LandlordHome/LandlordHome.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../../../hooks/reduxHooks";
+import styles from "./Landlord.module.scss";
+
+export const LandlordHome: React.FC = () => {
+
+    const dispatch = useAppDispatch();
+    const { firstName, lastName } = useAppSelector(state => state.auth);
+
+    return (
+        <div className={styles.landlordHomeContainer}>
+            <h1>Welcome to PropertyPro, {firstName} {lastName}!</h1>
+            <hr />
+            <div className={styles.actionCards}>
+                <div className={styles.actionCard}>
+                    <h3>Create property</h3>
+                    <hr />
+                    <p>List a property of yours, for rental, so tenants can rent it!</p>
+                    <Link to={"/property/create"} className={styles.createPropertyBtn}>Create property</Link>
+                </div>
+                <div className={styles.actionCard}>
+                    <h3>My properties</h3>
+                    <hr />
+                    <p>Take a look at your own listed properties in PropertyPro!</p>
+                    <Link to={"/my-properties"} className={styles.myPropertiesBtn}>My properties</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
