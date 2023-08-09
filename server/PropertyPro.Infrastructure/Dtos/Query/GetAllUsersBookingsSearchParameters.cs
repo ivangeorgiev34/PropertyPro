5a7bbb4f044e6252ffed6d620d14fdd37e23c573
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-09 16:54:52 +0300

    Add GetAllUsersBookingsSearchParameters DTO for search params

---
+++ server/PropertyPro.Infrastructure/Dtos/Query/GetAllUsersBookingsSearchParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Query
+{
+	public class GetAllUsersBookingsSearchParameters
+	{
+        public string? Title { get; set; }
+    }
+}
