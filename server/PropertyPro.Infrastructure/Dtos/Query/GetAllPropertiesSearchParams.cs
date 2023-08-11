eff538eaac5102bb911b174b4f88034a4139a5a4
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-11 12:28:45 +0300

    Add GetAllPropertiesSearchParams DTO

---
+++ server/PropertyPro.Infrastructure/Dtos/Query/GetAllPropertiesSearchParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPro.Infrastructure.Dtos.Query
+{
+	public class GetAllPropertiesSearchParams
+	{
+		public string? Title { get; set; }
+		public string? Town { get; set; }
+		public string? Country { get; set; }
+		public int Page { get; set; } = 1;
+    }
+}
