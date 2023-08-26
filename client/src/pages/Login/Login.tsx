efafb05e2b13753383e5404a7b7262f68bb8d854
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-26 15:22:22 +0300

    Add data-testid attribute to form and password input

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,12 +1,12 @@
 import React, { FormEventHandler, FormHTMLAttributes, useState } from "react";
-import styles from "./Login.module.scss"
+import styles from "./Login.module.scss";
 import { Link } from "react-router-dom";
 import { userLogin, userRegister } from "../../services/authenticationService";
 import { useForm } from "../../hooks/useForm/useForm";
 import { login } from "../../store/auth";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import { useNavigate, NavigateOptions } from "react-router-dom";
-import { toggleLoaderOff, toggleLoaderOn } from "../../store/loader"
+import { toggleLoaderOff, toggleLoaderOn } from "../../store/loader";
 import ILoginForm from "../../interfaces/ILoginForm";
 import { useError } from "../../hooks/useError/useError";
 import { emailValidation } from "../../validators/emailValidation";
@@ -19,8 +19,8 @@ export const Login: React.FC = () => {
     const [errors, setErrors] = useState<string[]>([]);
     const navigate = useNavigate();
 
-    const { token } = useAppSelector((state) => state.auth)
-    const dispatch = useAppDispatch()
+    const { token } = useAppSelector((state) => state.auth);
+    const dispatch = useAppDispatch();
 
     const { formValues, onFormChange } = useForm<ILoginForm>({
         email: "",
@@ -71,7 +71,7 @@ export const Login: React.FC = () => {
             dispatch(toggleLoaderOff());
 
         }
-    }
+    };
 
     const areFormValuesIncorrect = (): boolean => {
         for (let key in formErrors) {
@@ -91,7 +91,7 @@ export const Login: React.FC = () => {
 
     return (
         <div className={styles.formWrapper}>
-            <form className={styles.loginForm} onSubmit={onLogInFormSubmit}>
+            <form className={styles.loginForm} onSubmit={onLogInFormSubmit} data-testid="login-form">
                 <h2 className={styles.loginHeading}>Sign in to PropertyPro</h2>
                 <div className={styles.emailContainer}>
                     <label htmlFor="email">Email:</label>
@@ -101,7 +101,10 @@ export const Login: React.FC = () => {
                 </div>
                 <div className={styles.passwordContainer}>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" placeholder="Password..." value={formValues.password} onChange={onFormChange}
+                    <input type="password" name="password" placeholder="Password..."
+                        data-testid="password-input"
+                        value={formValues.password}
+                        onChange={onFormChange}
                         onBlur={(e) => onFormErrorChange(e, passwordValidation(formValues.password))} />
                     {<p className={styles.error}>{formErrors.password}</p>}
                 </div>
@@ -125,4 +128,4 @@ export const Login: React.FC = () => {
             </form>
         </div >
     );
-}
+};
