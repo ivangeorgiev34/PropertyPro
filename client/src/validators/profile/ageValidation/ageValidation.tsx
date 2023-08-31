5352a87eadea3733d073ac653639d42df65de22b
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-08-31 16:45:01 +0300

    Add check if age is whole number in ageValidation

--- client/src/validators/profile/ageValidation/ageValidation.tsx
+++ client/src/validators/profile/ageValidation/ageValidation.tsx
@@ -2,9 +2,11 @@ export const ageValidation = (age: number): string => {
 
     const result = age.toString().length === 0
         ? "Age is required"
-        : age <= 0
-            ? "Age should be a positive number"
-            : "";
+        : Number.isInteger(age) === false
+            ? "Age should be a whole number"
+            : age <= 0
+                ? "Age should be a positive number"
+                : "";
 
     return result;
 };
