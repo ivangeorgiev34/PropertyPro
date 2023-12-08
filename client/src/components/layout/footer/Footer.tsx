8589f45559a4a7e8ca3beb7ddb0ad4c075258013
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-12-08 14:08:17 +0200

    Add anchor wrapper around social icons

--- client/src/components/layout/footer/Footer.tsx
+++ client/src/components/layout/footer/Footer.tsx
@@ -2,16 +2,36 @@ import React from "react";
 import styles from "./Footer.module.scss";
 
 export const Footer: React.FC = () => {
-    return (
-        <footer id={styles.footer}>
-            <span>© 2023 PropertyPro</span>
-            <ul className={styles.socialsList}>
-                <li><i className="fa-brands fa-square-facebook fa-2xl"></i></li>
-                <li><i className="fa-brands fa-square-instagram fa-2xl"></i></li>
-                <li><i className="fa-brands fa-square-twitter fa-2xl"></i></li>
-                <li><i className="fa-brands fa-square-youtube fa-2xl"></i></li>
-                <li><i className="fa-brands fa-linkedin fa-2xl"></i></li>
-            </ul>
-        </footer>
-    );
+  return (
+    <footer id={styles.footer}>
+      <span>© 2023 PropertyPro</span>
+      <ul className={styles.socialsList}>
+        <li>
+          <a href="https://www.facebook.com/">
+            <i className="fa-brands fa-square-facebook fa-2xl"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/">
+            <i className="fa-brands fa-square-instagram fa-2xl"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com/">
+            <i className="fa-brands fa-square-twitter fa-2xl"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/">
+            <i className="fa-brands fa-square-youtube fa-2xl"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/">
+            <i className="fa-brands fa-linkedin fa-2xl"></i>
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
