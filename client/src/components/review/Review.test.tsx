c6a8f571658f6ca14dc4399b3e96cd21e7d87c3f
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-12-09 20:07:54 +0200

    Add onRevewDelete function in Review component invocation

--- client/src/components/review/Review.test.tsx
+++ client/src/components/review/Review.test.tsx
@@ -1,4 +1,10 @@
-import { fireEvent, getByText, render, screen, waitFor } from "@testing-library/react";
+import {
+  fireEvent,
+  getByText,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import IReview from "../../interfaces/review/IReview";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
@@ -8,199 +14,196 @@ import { login } from "../../store/auth";
 import * as reviewService from "../../services/reviewService";
 
 describe("Review", () => {
+  const reviewProps: IReview = {
+    id: "reviewId",
+    stars: 3,
+    description: "desc",
+    tenant: {
+      id: "tenantId",
+      firstName: "tenant",
+      middleName: "tenant",
+      lastName: "tenant",
+      age: 34,
+      gender: "male",
+      profilePicture: null,
+    },
+  };
 
-    const reviewProps: IReview = {
-        id: "reviewId",
-        stars: 3,
-        description: "desc",
-        tenant: {
-            id: "tenantId",
-            firstName: "tenant",
-            middleName: "tenant",
-            lastName: "tenant",
-            age: 34,
-            gender: "male",
-            profilePicture: null
-        }
-    };
+  const onReviewDelete = (): void => {};
 
-    afterEach(() => {
-        jest.restoreAllMocks();
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("yellow stars and black stars are correctly loaded", () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Review {...reviewProps} onReviewDelete={onReviewDelete} />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    const yellowStarsCount = screen.getAllByTestId(/yellow-star/i).length;
+    const blackStarsCount = screen.getAllByTestId(/black-star/i).length;
+
+    expect(yellowStarsCount).toBe(3);
+    expect(blackStarsCount).toBe(2);
+  });
+
+  test("delete button click error message span should be empty when window confirm is false", () => {
+    store.dispatch(
+      login({
+        id: "tenantId",
+      })
+    );
+
+    jest.spyOn(window, "confirm").mockReturnValue(false);
+
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Review {...reviewProps} onReviewDelete={onReviewDelete} />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    const deleteBtn = screen.getByRole("button", {
+      name: /delete/i,
     });
 
-    test("yellow stars and black stars are correctly loaded", () => {
+    fireEvent.click(deleteBtn);
 
-        render(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Review {...reviewProps} />
-                </BrowserRouter>
-            </Provider>
-        );
+    const deleteError = screen.getByTestId("delete-error").textContent;
 
-        const yellowStarsCount = screen.getAllByTestId(/yellow-star/i).length;
-        const blackStarsCount = screen.getAllByTestId(/black-star/i).length;
+    expect(deleteError).toBe("");
+  });
 
-        expect(yellowStarsCount).toBe(3);
-        expect(blackStarsCount).toBe(2);
+  test("delete button click error message span should not be empty when window confirm is true", () => {
+    store.dispatch(
+      login({
+        id: "tenantId",
+      })
+    );
+
+    jest.spyOn(window, "confirm").mockReturnValue(true);
+
+    jest.spyOn(reviewService, "deleteReviewById").mockResolvedValue({
+      status: "Error",
+      message: "error message",
     });
 
-    test("delete button click error message span should be empty when window confirm is false", () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Review {...reviewProps} onReviewDelete={onReviewDelete} />
+        </BrowserRouter>
+      </Provider>
+    );
 
-        store.dispatch(login({
-            id: "tenantId"
-        }));
-
-        jest.spyOn(window, "confirm")
-            .mockReturnValue(false);
-
-        render(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Review {...reviewProps} />
-                </BrowserRouter>
-            </Provider>
-        );
-
-        const deleteBtn = screen.getByRole("button", {
-            name: /delete/i
-        });
-
-        fireEvent.click(deleteBtn);
-
-        const deleteError = screen.getByTestId("delete-error").textContent;
-
-        expect(deleteError).toBe("");
+    const deleteBtn = screen.getByRole("button", {
+      name: /delete/i,
     });
 
-    test("delete button click error message span should not be empty when window confirm is true", () => {
+    fireEvent.click(deleteBtn);
 
-        store.dispatch(login({
-            id: "tenantId"
-        }));
+    waitFor(() => {
+      const deleteError = screen.getByText(/error message/i);
 
-        jest.spyOn(window, "confirm")
-            .mockReturnValue(true);
+      expect(deleteError).toBeInTheDocument();
+    });
+  });
 
-        jest.spyOn(reviewService, "deleteReviewById")
-            .mockResolvedValue({
-                status: "Error",
-                message: "error message"
-            });
+  test("delete button click should redirect to home page when request is successful", () => {
+    store.dispatch(
+      login({
+        id: "tenantId",
+      })
+    );
 
-        render(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Review {...reviewProps} />
-                </BrowserRouter>
-            </Provider>
-        );
+    jest.spyOn(window, "confirm").mockReturnValue(true);
 
-        const deleteBtn = screen.getByRole("button", {
-            name: /delete/i
-        });
-
-        fireEvent.click(deleteBtn);
-
-        waitFor(() => {
-            const deleteError = screen.getByText(/error message/i);
-
-            expect(deleteError).toBeInTheDocument();
-        });
+    jest.spyOn(reviewService, "deleteReviewById").mockResolvedValue({
+      status: "Success",
+      message: "success message",
     });
 
-    test("delete button click should redirect to home page when request is successful", () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Review {...reviewProps} onReviewDelete={onReviewDelete} />
+        </BrowserRouter>
+      </Provider>
+    );
 
-        store.dispatch(login({
-            id: "tenantId"
-        }));
-
-        jest.spyOn(window, "confirm")
-            .mockReturnValue(true);
-
-        jest.spyOn(reviewService, "deleteReviewById")
-            .mockResolvedValue({
-                status: "Success",
-                message: "success message"
-            });
-
-        render(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Review {...reviewProps} />
-                </BrowserRouter>
-            </Provider>
-        );
-
-        const deleteBtn = screen.getByRole("button", {
-            name: /delete/i
-        });
-
-        fireEvent.click(deleteBtn);
-
-        const actualResult = window.location.href;
-        const expectedResult = "http://localhost/";
-
-        expect(actualResult).toBe(expectedResult);
+    const deleteBtn = screen.getByRole("button", {
+      name: /delete/i,
     });
 
-    test("profile picture should not show when tenant's profile picture is null", () => {
+    fireEvent.click(deleteBtn);
 
-        render(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Review {...reviewProps} />
-                </BrowserRouter>
-            </Provider>
-        );
+    const actualResult = window.location.href;
+    const expectedResult = "http://localhost/";
 
-        const tenantProfilePicture: HTMLImageElement = screen.getByRole("img");
+    expect(actualResult).toBe(expectedResult);
+  });
 
-        const actualResult = tenantProfilePicture.src;
-        const expectedResult = "https://thumbs.dreamstime.com/b/user-icon-flat-style-isolated-grey-background-user-icon-flat-style-isolated-grey-background-your-design-logo-131213475.jpg";
+  test("profile picture should not show when tenant's profile picture is null", () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Review {...reviewProps} onReviewDelete={onReviewDelete} />
+        </BrowserRouter>
+      </Provider>
+    );
 
-        expect(actualResult).toBe(expectedResult);
+    const tenantProfilePicture: HTMLImageElement = screen.getByRole("img");
+
+    const actualResult = tenantProfilePicture.src;
+    const expectedResult =
+      "https://thumbs.dreamstime.com/b/user-icon-flat-style-isolated-grey-background-user-icon-flat-style-isolated-grey-background-your-design-logo-131213475.jpg";
+
+    expect(actualResult).toBe(expectedResult);
+  });
+
+  test("profile picture should show when tenant's profile picture is not null", () => {
+    reviewProps.tenant.profilePicture = "";
+
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Review {...reviewProps} onReviewDelete={onReviewDelete} />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    const tenantProfilePicture: HTMLImageElement = screen.getByRole("img");
+
+    const actualResult = tenantProfilePicture.src;
+    const expectedResult =
+      "https://thumbs.dreamstime.com/b/user-icon-flat-style-isolated-grey-background-user-icon-flat-style-isolated-grey-background-your-design-logo-131213475.jpg";
+
+    expect(actualResult).not.toBe(expectedResult);
+  });
+
+  test("edit link should redirect to review edit page", () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Review {...reviewProps} onReviewDelete={onReviewDelete} />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    const editLink = screen.getByRole("link", {
+      name: /edit/i,
     });
 
-    test("profile picture should show when tenant's profile picture is not null", () => {
+    fireEvent.click(editLink);
 
-        reviewProps.tenant.profilePicture = "";
+    const actualResult = window.location.href;
+    const expectedResult = "http://localhost/review/edit/reviewId";
 
-        render(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Review {...reviewProps} />
-                </BrowserRouter>
-            </Provider>
-        );
-
-        const tenantProfilePicture: HTMLImageElement = screen.getByRole("img");
-
-        const actualResult = tenantProfilePicture.src;
-        const expectedResult = "https://thumbs.dreamstime.com/b/user-icon-flat-style-isolated-grey-background-user-icon-flat-style-isolated-grey-background-your-design-logo-131213475.jpg";
-
-        expect(actualResult).not.toBe(expectedResult);
-    });
-
-    test("edit link should redirect to review edit page", () => {
-
-        render(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Review {...reviewProps} />
-                </BrowserRouter>
-            </Provider>
-        );
-
-        const editLink = screen.getByRole("link", {
-            name: /edit/i
-        });
-
-        fireEvent.click(editLink);
-
-        const actualResult = window.location.href;
-        const expectedResult = "http://localhost/review/edit/reviewId";
-
-        expect(actualResult).toBe(expectedResult);
-    });
+    expect(actualResult).toBe(expectedResult);
+  });
 });
